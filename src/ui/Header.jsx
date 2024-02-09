42ac5aedcf3ae66fb2a3fbf167e0e58d0cbd8918
3nonch <enonch@hotmail.com>
2024-02-10 00:35:45 +0300

    initial features

---
+++ src/ui/Header.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Header() {
+  return (
+    <header>
+        <Link to='/'>Pizza Company</Link>
+        <p>Enonch</p>
+    </header>
+  )
+}
