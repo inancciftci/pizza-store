c4ac67b4477047b4c07858f61b1cc828ae97f531
3nonch <enonch@hotmail.com>
2024-02-10 15:18:15 +0300

    created SearchOrder component

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,11 +1,13 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
 import { Link } from 'react-router-dom'
+import SearchOrder from '../features/order/SearchOrder'
 
 export default function Header() {
   return (
     <header>
         <Link to='/'>Pizza Company</Link>
+        <SearchOrder />
         <p>Enonch</p>
     </header>
   )
