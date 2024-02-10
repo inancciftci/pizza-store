606047285a1bea95ff55a256e2230a38c6eec95e
3nonch <enonch@hotmail.com>
2024-02-10 14:56:27 +0300

    Error handling & Loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom"
 import Home from "./ui/Home"
-import Menu from "./features/menu/Menu"
+import Error from "./ui/Error"
+import Menu, {loader as menuLoader} from "./features/menu/Menu"
 import Cart from "./features/cart/Cart"
 import CreateOrder from "./features/order/CreateOrder"
 import Order from "./features/order/Order"
@@ -10,6 +11,7 @@ import AppLayout from "./ui/AppLayout"
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    errorElement: <Error />,
     children: [
       {
     path: '/',
@@ -17,7 +19,9 @@ const router = createBrowserRouter([
   },
   {
     path: '/menu',
-    element: <Menu />
+    element: <Menu />,
+    loader: menuLoader,
+    errorElement: <Error />
   },
   {
     path: '/cart',
